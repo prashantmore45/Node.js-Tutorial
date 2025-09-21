0517dd8026eee6c12a64dec4ba56796da48d7284
prashantmore45 <prashantmorepm05@gmail.com>
2025-09-21 17:01:34 +0530

    Learned the use of export and import  of ES module

---
+++ ESmodule/app.js
@@ -0,0 +1,14 @@
+import { add, subs, mult
+    , div
+ } from "./math.js";
+
+
+ console.log(add(34, 116));
+
+ console.log(subs(55, 45));
+
+ console.log(mult(66, 54));
+
+ console.log(div(45, 5));
+
+ 
